1a6041f9a8ed5672f67ae8564e625edd18f43039
iampuiiyuen <17956502+puiiyuen@users.noreply.github.com>
2018-12-09 13:05:58 -0400

    Update Register.java

--- src/com/User/Register.java
+++ src/com/User/Register.java
@@ -1,3 +1,11 @@
+/*
+As a server-side developer, I want to provide user login and register, so that residents can use their own account to do some operations (e.g. food feedback). 
+Index#: 42 User Login Server (59 Java EE 60 MySQL) 
+Complexity#: 8 
+*/
+
+//register
+
 package com.User;
 
 import java.io.UnsupportedEncodingException;
@@ -15,10 +23,11 @@ public class Register {
 
         String encryptPasswd = Base64.getEncoder().encodeToString(password.getBytes("utf-8"));
 
-
+        //check if there is an account
         if (conDBS.search("select * from userinfo where email='" + email + "'", "email"))
             insertSucc = false;
         else {
+            //if not create a new account
             insertSucc = conDBS.insert("insert into userinfo (username,email,passwd,type) values('" + username + "','"
                     + email + "','" + encryptPasswd + "','user')");
         }
